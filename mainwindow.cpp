0fd7c3132f118a325b7b1eea648cc43e6abe7020
Salieri <kirito.hikaia@gmail.com>
2021-10-29 21:19:50 +0200

    Finished sign in function (30 pts)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +12,54 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+bool MainWindow::LogIn(QString userName, QString userPassword) {
+
+for (int i=0; i<=2;i++)
+{
+    if (userName==Names[i]){
+        NameFound=true;
+        break;}
+    else {
+        NameFound=false;
+    }
+}
+for (int j=0; j<=3;j++)
+{
+    if (userPassword.toInt()==Passwords[j]){
+        Passwordfound=true;
+    break;}
+    else {
+        Passwordfound=false;
+    }
+}
+
+if (NameFound==true && Passwordfound==true){
+return true;
+}
+else
+    return false;
+
+}
+
+void MainWindow::on_pushButton_SignIn_clicked()
+{
+
+    userName = ui->Label_Name->text();
+    userPassword = ui->Label_Password->text();
+    bool signin = LogIn (userName, userPassword);
+
+    if (signin==true)
+        ui->Label_LogIn_Message->setText("Signed in Successfully.");
+        else {
+        if (NameFound==true && Passwordfound==false)
+            ui->Label_LogIn_Message->setText("Name is corrcet but password is wrong.");
+        else if (NameFound == false)
+            ui->Label_LogIn_Message->setText("Name does not exist.");
+
+
+
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QString>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,7 +15,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+bool LogIn(QString userName, QString userPassword); //Function to log in
+
+bool NameFound =true;
+        bool  Passwordfound = true;
+
 private:
     Ui::MainWindow *ui;
+    QString userName;
+    QString userPassword;
+    QString Names [3] = {"Ahmed", "Mariam", "Eman"};
+    int Passwords [3] = {1,2,3};
+
+
+private slots:
+    void on_pushButton_SignIn_clicked();
+
+
 };
 #endif // MAINWINDOW_H
