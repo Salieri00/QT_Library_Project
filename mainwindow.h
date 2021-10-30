fb56cbc3a5fbe63ec25b3f8228de611ba271198a
Salieri <kirito.hikaia@gmail.com>
2021-10-30 02:38:07 +0200

    Finished everything, only the validation of the balance left

--- mainwindow.h
+++ mainwindow.h
@@ -17,21 +17,33 @@ public:
 
 
 bool LogIn(QString userName, QString userPassword); //Function to log in
+bool isNumber (QString& str); //Function to check if number
 
 bool NameFound =true;
-        bool  Passwordfound = true;
+bool  Passwordfound = true;
+bool signin;
 
 private:
     Ui::MainWindow *ui;
     QString userName;
     QString userPassword;
+    int user = 0;
     QString Names [3] = {"Ahmed", "Mariam", "Eman"};
     int Passwords [3] = {1,2,3};
+    QString IDs[3]= {"100", "101", "102"};
+    QString userBalance;
+    int Acc_Balance [3] = {0,0,0};
+    QString BookName;
+    int BookIndex = 0;
+    int BookPrice [3] = {450, 390, 500};
+
 
 
 private slots:
     void on_pushButton_SignIn_clicked();
-
-
+    void on_pushButton_balance_clicked();
+    void on_pushButton_buy_clicked();
+    void on_comboBox_currentTextChanged(const QString &arg1);
+    void on_pushButton_clicked();
 };
 #endif // MAINWINDOW_H
